58f5b43d609f12c9be9f431b8256501017583d89
Alessandro Queiroz <queirozalessandro1@gmail.com>
2021-03-06 17:45:29 -0300

    MongoDB configuration

---
+++ pages/api/user.tsx
@@ -0,0 +1,43 @@
+import { NextApiRequest, NextApiResponse } from 'next';
+import connect from '../../utils/database'
+
+interface ErrorResponseType {
+  error: string;
+}
+
+interface SuccessResponseType {
+  _id: string;
+  name: string;
+  email: string;
+  cellphone: string;
+  type: string;
+
+}
+
+export default async (
+  req: NextApiRequest, 
+  res: NextApiResponse<ErrorResponseType | SuccessResponseType>
+  ): Promise<void> => {
+    const { name, email, cellphone, type } = req.body;
+    
+    if (!name || !email || !cellphone || !type) {
+      res.status(400).json({ error: "Missing body parameter" });
+      return;
+    }
+    
+    const { db } = await connect();
+
+    if (req.method === "POST") {
+      const response = await db.collection('users').insertOne({
+        name, 
+        email,
+        cellphone, 
+        type
+      });
+      res.status(200).json(response.ops[0]);
+    } else {
+      res.status(400).json({ error: name })
+    }
+
+
+};
